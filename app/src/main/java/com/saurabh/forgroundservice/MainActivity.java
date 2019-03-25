5e54fc5029d575625215e61381019080fe8c4933
Saurabh <sksaurabh021@gmail.com>
2019-03-25 23:03:17 +0530

    Forground service example

---
+++ app/src/main/java/com/saurabh/forgroundservice/MainActivity.java
@@ -0,0 +1,37 @@
+package com.saurabh.forgroundservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Android Foreground Service Example.");
+
+        Button startServiceButton = (Button)findViewById(R.id.start_foreground_service_button);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyForeGroundService.class);
+                intent.setAction(MyForeGroundService.ACTION_START_FOREGROUND_SERVICE);
+                startService(intent);
+            }
+        });
+
+        Button stopServiceButton = (Button)findViewById(R.id.stop_foreground_service_button);
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyForeGroundService.class);
+                intent.setAction(MyForeGroundService.ACTION_STOP_FOREGROUND_SERVICE);
+                startService(intent);
+            }
+        });
+    }
+}
